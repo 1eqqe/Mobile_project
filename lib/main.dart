36bc9c75bcfeead3bb69fe855691866a3193dae8
1eqqe <mr.baydali@mail.ru>
2023-03-05 13:20:49 +0600

    finished project

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:mobile_project/screens/auth/auth_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+main() async {
+  runApp(GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: AuthScreen(),
+  ));
+}
